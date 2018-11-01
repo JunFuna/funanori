ec061debdd71e7472a1402e7e37b3e3a462efc95
= <funatsuki-jy3@outlook.jp>
2018-11-01 17:02:49 +0900

    first commit

---
+++ sorce/sample.py
@@ -0,0 +1,16 @@
+
+
+def main():
+    print("hello world")
+    print(max(4, 5))
+
+
+def max(a, b):
+    if a < b:
+        return b
+    else:
+        return
+
+
+if __name__ == '__main__':
+    main()
